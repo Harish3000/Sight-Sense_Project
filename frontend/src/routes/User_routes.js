625be6617de0a0a7b3b587f939db56d3b7f8374e
IT21258480 <100502665+Pathumdissanayake@users.noreply.github.com>
2023-09-05 18:30:17 +0530

    Login created

--- frontend/src/routes/User_routes.js
+++ frontend/src/routes/User_routes.js
@@ -1,17 +1,15 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
 import LoginPage from '../pages/User_pages/Login_home';
 import RegisterPage from '../pages/User_pages/Register_home';
 import ProfileHome from '../pages/User_pages/User_profile';
 
 export default function User_routes() {
-  return (
-    <Routes>
-      <Route>
+    return (
+      <Routes>
       <Route  path="/" element={<LoginPage/>}/>
       <Route path="/register" element={<RegisterPage/>}/>
       <Route path="/ProfileHome" element={<ProfileHome/>}/>
-      </Route>
-    </Routes>
-  );
-}
+      </Routes>
+    )
+  }
