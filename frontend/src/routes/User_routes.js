4a4ce53af2029258328a78d95784f1812f16002f
IT21258480 <100502665+Pathumdissanayake@users.noreply.github.com>
2023-09-05 18:27:22 +0530

    Register and login routes added

--- frontend/src/routes/User_routes.js
+++ frontend/src/routes/User_routes.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import LoginPage from '../pages/User_pages/Login_home';
+import RegisterPage from '../pages/User_pages/Register_home';
+import ProfileHome from '../pages/User_pages/User_profile';
 
 export default function User_routes() {
   return (
     <Routes>
-      <Route></Route>
+      <Route>
+      <Route  path="/" element={<LoginPage/>}/>
+      <Route path="/register" element={<RegisterPage/>}/>
+      <Route path="/ProfileHome" element={<ProfileHome/>}/>
+      </Route>
     </Routes>
   );
 }
