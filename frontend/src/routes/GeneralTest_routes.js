8d2ccddac9871b0cd2f4cb40700f9d3079bf698f
Rehantp <rehamc123@gmail.com>
2023-09-05 17:53:28 +0530

    init Components

--- frontend/src/routes/GeneralTest_routes.js
+++ frontend/src/routes/GeneralTest_routes.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Test from "../components/GeneralTest_components/test";
+import QuizInstruction from "../components/GeneralTest_components/Quiz/QuizInstruction";
 
 export default function GeneralTest_routes() {
   return (
     <Routes>
-      <Route></Route>
+      <Route path="/general-test/test" element={<Test />} />
+      <Route
+        path="/general-test/quiz-instruction"
+        element={<QuizInstruction />}
+      />
     </Routes>
   );
 }
