b578784cb75ef9b79137926b01d335405dbc0e80
Rehantp <rehamc123@gmail.com>
2023-09-07 08:25:24 +0530

    Routes

---
+++ backend/routes/Generaltest/GeneralTest.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+let Test = require("../../models/GeneralTest_Modal");
+const { route } = require("../User/User");
+
+//Add test
+router.route("/add").post((req, res) => {
+  const test_name = "General Test";
+  const user_id = generateRandomUserId();
+  const test_date = new Date();
+  const test_score = Number(req.body.test_score);
+
+  const newTest = new Test({
+    test_name,
+    user_id,
+    test_date,
+    test_score,
+  });
+
+  newTest
+    .save()
+    .then(() => {
+      res.json("Test added!");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
