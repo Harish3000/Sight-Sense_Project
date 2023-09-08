a4115bcd4f45120eca68fef5483e005150469262
Harish <129611646+Harish3000@users.noreply.github.com>
2023-09-08 12:28:26 +0530

    added camera function

--- frontend/src/components/AdvancedTest_components/ImageUploader.js
+++ frontend/src/components/AdvancedTest_components/ImageUploader.js
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
-import { Upload, Button, Spin, message, Image } from "antd";
-import { UploadOutlined } from "@ant-design/icons";
+import React, { useState, useRef } from "react";
+import { Upload, Button, Spin, message, Image, Modal } from "antd";
+import { UploadOutlined, CameraOutlined } from "@ant-design/icons";
 import axios from "axios";
-import "./ImageUploader.css"; // Import the external CSS file
+import Webcam from "react-webcam";
+import "./ImageUploader.css";
 
 function ImageUploader() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [predictionResult, setPredictionResult] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [cameraVisible, setCameraVisible] = useState(false);
+  const [disableButtons, setDisableButtons] = useState(false);
+  const webcamRef = useRef(null);
 
   const handleFileChange = (info) => {
     if (info.file.status === "done") {
@@ -49,12 +53,46 @@ function ImageUploader() {
       );
 
       setPredictionResult(formattedPredictions.join("\n"));
+      setDisableButtons(true);
+
+      // Close the camera modal after uploading
+      closeCamera();
     } catch (error) {
       console.error("Error:", error);
       message.error("An error occurred while processing the image.");
     } finally {
       setLoading(false);
     }
+  };
+
+  const openCamera = () => {
+    setCameraVisible(true);
+  };
+
+  const closeCamera = () => {
+    setCameraVisible(false);
+  };
+
+  const takePicture = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setSelectedFile(dataURLtoFile(imageSrc, "snapshot.png"));
+    closeCamera();
+  };
+
+  const redoTest = () => {
+    window.location.reload();
+  };
+
+  const dataURLtoFile = (dataUrl, fileName) => {
+    const arr = dataUrl.split(",");
+    const mime = arr[0].match(/:(.*?);/)[1];
+    const bstr = atob(arr[1]);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], fileName, { type: mime });
   };
 
   const renderImagePreview = () => {
@@ -83,6 +121,15 @@ function ImageUploader() {
       <h2>Image Preview:</h2>
       {renderImagePreview()}
       <div style={{ marginTop: "20px" }}>
+        <Button
+          icon={<UploadOutlined />}
+          onClick={openCamera}
+          disabled={disableButtons}
+        >
+          Open Camera
+        </Button>
+      </div>
+      <div style={{ marginTop: "20px" }}>
         <Upload
           accept="image/*"
           customRequest={() => {}}
@@ -92,11 +139,17 @@ function ImageUploader() {
             handleFileChange(info);
           }}
         >
-          <Button icon={<UploadOutlined />}>Select Image</Button>
+          <Button icon={<UploadOutlined />} disabled={disableButtons}>
+            Select Image
+          </Button>
         </Upload>
       </div>
       <div style={{ marginTop: "10px" }}>
-        <Button type="primary" onClick={handleUpload} disabled={!selectedFile}>
+        <Button
+          type="primary"
+          onClick={handleUpload}
+          disabled={!selectedFile || disableButtons}
+        >
           Upload
         </Button>
       </div>
@@ -111,6 +164,41 @@ function ImageUploader() {
           <pre>{predictionResult}</pre>
         </div>
       )}
+      {disableButtons && (
+        <div style={{ marginTop: "20px" }}>
+          <Button type="primary" onClick={redoTest}>
+            Re-Do Test
+          </Button>
+        </div>
+      )}
+      <Modal
+        title="Camera"
+        visible={cameraVisible}
+        onCancel={closeCamera}
+        footer={[
+          <Button key="back" onClick={closeCamera}>
+            Cancel
+          </Button>,
+          <Button
+            key="submit"
+            type="primary"
+            onClick={takePicture}
+            disabled={disableButtons}
+          >
+            Take Picture
+          </Button>,
+        ]}
+        width={800}
+        bodyStyle={{ height: 500 }}
+      >
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/png"
+          width={768}
+          height={476}
+        />
+      </Modal>
     </div>
   );
 }
