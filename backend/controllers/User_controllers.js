5e2147e023cb7d39594e3bb6a42da2efbdbb8cfc
IT21258480 <100502665+Pathumdissanayake@users.noreply.github.com>
2023-09-08 10:28:00 +0530

    Update User_controllers.js

--- backend/controllers/User_controllers.js
+++ backend/controllers/User_controllers.js
@@ -3,11 +3,28 @@ const mongoose = require("mongoose");
 
 // create a new Item
 const createUser = async (req, res) => {
-  const { name } = req.body;
+  const { firstname,
+          lastname,
+          contact,
+          addLine1,
+          addLine2,
+          addLine3,
+          gender,
+          email,
+          pwd
+   } = req.body;
 
   //add the item to the database
   try {
-    const obj = await User.create({ name });
+    const obj = await User.create({ firstname,
+      lastname,
+      contact,
+      addLine1,
+      addLine2,
+      addLine3,
+      gender,
+      email,
+      pwd });
     res.status(200).json(obj);
   } catch (error) {
     res.status(400).json({ error: error.message });
