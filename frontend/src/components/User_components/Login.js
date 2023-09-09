2f212d31c74e52081d376e0551afe109c506d71c
IT21258480 <100502665+Pathumdissanayake@users.noreply.github.com>
2023-09-09 17:38:26 +0530

    styled login errors

--- frontend/src/components/User_components/Login.js
+++ frontend/src/components/User_components/Login.js
@@ -5,81 +5,116 @@ import { ToastContainer as ReactToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useLogIn } from "../../hooks/User_hooks/useLogIn";
 import validator from "validator";
+import { Input, Button, Alert } from "antd";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [errorMessage, setErrorMessage] = useState(""); // Declare errorMessage state
-    const { login, isLoading } = useLogIn();
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(""); // Declare errorMessage state
+  const { login, isLoading } = useLogIn();
+  const navigate = useNavigate();
 
-    const handleLoginFormSubmit = async (e) => {
-        e.preventDefault();
-        setErrorMessage(""); // Clear any previous error message
+  const handleLoginFormSubmit = async (e) => {
+    e.preventDefault();
+    setErrorMessage(""); // Clear any previous error message
 
-        if (!validator.isEmail(email)) {
-            setErrorMessage("Email is not valid");
-            return;
-        }
+    if (!validator.isEmail(email)) {
+      setErrorMessage("Email is not valid");
+      return;
+    }
 
-        if (!email || !password) {
-            setErrorMessage("Please fill out all the fields");
-            return;
-        }
+    if (!email || !password) {
+      setErrorMessage("Please fill out all the fields");
+      return;
+    }
 
-        try {
-          await login(email, password);
-      
-          // If no errors occurred, navigate to the home page
-          navigate("/home");
-      } catch (err) {
-          // Handle errors from the hook
-          setErrorMessage(err.message || "An error occurred. Please try again.");
-      }
-    };
+    try {
+      await login(email, password);
 
-    return (
-        <div className="login-body">
-            <div className="div-for-login-box">
-                <div className="login">
-                    <div className="login-heading-div">
-                        <h2>Login</h2>
-                    </div>
+      // If no errors occurred, navigate to the home page
+      navigate("/home");
+    } catch (err) {
+      // Handle errors from the hook
+      setErrorMessage(err.message || "An error occurred. Please try again.");
+    }
+  };
 
-                    <div className="login-form-container-div">
-                        <form onSubmit={handleLoginFormSubmit}>
-                            <div>
-                                <label>Email:</label>
-                                <input
-                                    type="email"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                />
-                            </div>
-                            <div>
-                                <label>Password :</label>
-                                <input
-                                    type="password"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                />
-                            </div>
-                            <div className="btns-div">
-                                <button type="submit" disabled={isLoading}>
-                                    Login
-                                </button>
-                                <Link to="/register">
-                                    <button type="button">Register</button>
-                                </Link>
-                            </div>
-                            {errorMessage && <div className="err">{errorMessage}</div>}
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <ReactToastContainer />
+  return (
+    <div className="login-body">
+      <div className="div-for-login-box">
+        <div className="login">
+          <div className="login-heading-div">
+            <h2
+              style={{
+                fontFamily: "Poppins",
+                textAlign: "center",
+                paddingTop: "200px",
+                fontSize: "44px",
+              }}
+            >
+              Login
+            </h2>
+          </div>
+
+          <div
+            className="login-form-container-div"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "20vh",
+            }}
+          >
+            <form onSubmit={handleLoginFormSubmit}>
+              <div>
+                <label>Email:</label>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div>
+                <label>Password :</label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="btns-div">
+                <button type="submit" disabled={isLoading}>
+                  Login
+                </button>
+                <Link to="/register">
+                  <button type="button">Register</button>
+                </Link>
+              </div>
+              {errorMessage && (
+                <Alert
+                  message="Error"
+                  description={errorMessage}
+                  type="error"
+                  showIcon
+                  style={{
+                    position: "fixed",
+                    top: "30px",
+                    left: "50%",
+                    transform: "translateX(-50%)",
+                    zIndex: "9999",
+                  }}
+                  closable
+                  onClose={() => setErrorMessage("")}
+                />
+              )}
+            </form>
+          </div>
         </div>
-    );
+      </div>
+      <ReactToastContainer />
+    </div>
+  );
 };
 
 export default Login;
