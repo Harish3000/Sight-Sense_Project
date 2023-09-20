ff47c88dffdc763614f5d82174167daf93e45967
IT21258480 <100502665+Pathumdissanayake@users.noreply.github.com>
2023-09-20 18:01:26 +0530

    Update UserProfile.js

--- frontend/src/components/User_components/UserProfile.js
+++ frontend/src/components/User_components/UserProfile.js
@@ -251,7 +251,7 @@ export default function UserProfile() {
   };
 
   return (
-    <div style={{ padding: "40px", backgroundColor: "#CED2CC" }}>
+    <div style={{ paddingLeft: "20px", paddingTop:"20px", backgroundColor: "#CED2CC" }}>
       <div style={{ display: "flex" }}>
         <div
           style={{
