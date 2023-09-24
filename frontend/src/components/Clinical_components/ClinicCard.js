370c0fbd0268a92b0a539c133eba41af5a8595f7
K.G.I.H.Chamathka Kumbukgolla <harinichamathka14@gmail.com>
2023-09-25 04:23:02 +0530

    Remove files

--- frontend/src/components/Clinical_components/ClinicCard.js
+++ frontend/src/components/Clinical_components/ClinicCard.js
@@ -1,17 +1,16 @@
 import React from "react";
-import "../../styles/GeneralTest/Clinical/card.css";
+import "../../styles/GeneralTest/Clinical/homePage.css";
 
-function ClinicCard() {
- 
+function ClinicCard(props) {
   return (
     <div className="CardContainer">
       <div className="Card">
-        <h1 className="h1">Asiri centers</h1>
+        <h1 className="h1">{props.clinicName}</h1>
         <div className="description">
-          <h3 className="h3">Colombo</h3>
-          <h3 className="h3">011223623</h3>
+          <h3 className="h3">{props.clinicLocation}</h3>
+          <h3 className="h3">{props.clinicContact}</h3>
           <a href="" className="link">
-            www.asiriHospital.com
+            {props.clinicWebsite}
           </a>
         </div>
       </div>
