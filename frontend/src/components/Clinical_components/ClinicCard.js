75047d0ee877d9723a7cd1742176c1ce32218e6e
Harish <129611646+Harish3000@users.noreply.github.com>
2023-09-27 23:21:28 +0530

    UPDATED

--- frontend/src/components/Clinical_components/ClinicCard.js
+++ frontend/src/components/Clinical_components/ClinicCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../styles/GeneralTest/Clinical/homePage.css";
+import "../../styles/GeneralTest/Clinical/HomePage.css";
 
 function ClinicCard(props) {
   return (
